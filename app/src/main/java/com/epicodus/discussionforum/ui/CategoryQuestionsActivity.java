68180242ad3d200c50dfcb03a5aac9088e047b0a
Daniel Wulff and Inthrayuth Mahaphol <inthrayuth@>
2016-07-12 14:08:03 -0700

    chagned manifest for proper parent owner

--- app/src/main/java/com/epicodus/discussionforum/ui/CategoryQuestionsActivity.java
+++ app/src/main/java/com/epicodus/discussionforum/ui/CategoryQuestionsActivity.java
@@ -2,6 +2,8 @@ package com.epicodus.discussionforum.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.epicodus.discussionforum.R;
 import com.epicodus.discussionforum.models.Category;
@@ -10,15 +12,25 @@ import org.parceler.Parcels;
 
 import java.util.ArrayList;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class CategoryQuestionsActivity extends AppCompatActivity {
     ArrayList<Category> mCategories = new ArrayList<>();
+    @Bind(R.id.categoryTitle) TextView mCategoryTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_questions);
+        ButterKnife.bind(this);
 
-        mCategories = Parcels.unwrap(getIntent().getParcelableExtra("restaurants"));
+        mCategories = Parcels.unwrap(getIntent().getParcelableExtra("categories"));
         int startingPosition = Integer.parseInt(getIntent().getStringExtra("position"));
+
+        Log.d("Category", mCategories.get(startingPosition).getTitle());
+        Log.d("list, number", Integer.toString(startingPosition));
+
+        mCategoryTitle.setText(mCategories.get(startingPosition).getTitle());
     }
 }
--- app/src/main/java/com/epicodus/discussionforum/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/discussionforum/ui/MainActivity.java
@@ -14,11 +14,8 @@ import com.epicodus.discussionforum.R;
 import com.epicodus.discussionforum.adapters.FirebaseCategoryViewHolder;
 import com.epicodus.discussionforum.models.Category;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
